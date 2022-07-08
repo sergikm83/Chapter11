eaafc61c903af265c2e707f64913738ca0788c9c
Sergey Misko <spy_3462@mail.ru>
2022-07-08 15:56:54 +0400

    Добавьте файлы проекта.

---
+++ SimpleIndexer/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace SimpleIndexer
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
