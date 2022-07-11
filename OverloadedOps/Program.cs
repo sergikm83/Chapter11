8ec4aa4537b63fa7f581687b2740c16310c057d8
Sergey Misko <spy_3462@mail.ru>
2022-07-11 16:50:45 +0400

    изменил слово в строке с Fin на Fun

--- OverloadedOps/Program.cs
+++ OverloadedOps/Program.cs
@@ -6,7 +6,7 @@ namespace OverloadedOps
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("***** Fin with Overloaded Operators *****\n");
+            Console.WriteLine("***** Fun with Overloaded Operators *****\n");
             // создать 2 точки
             Point pointOne = new Point(100, 100);
             Point pointTwo = new Point(40, 40);
