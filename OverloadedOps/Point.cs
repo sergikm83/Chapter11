14ee95aa7c113dde39d780a80c02b5fcb81d44e4
Sergey Misko <spy_3462@mail.ru>
2022-07-11 16:47:28 +0400

    добавил класс Point

---
+++ OverloadedOps/Point.cs
@@ -0,0 +1,20 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace OverloadedOps
+{
+    public class Point
+    {
+        public int X { get; set; }
+        public int Y { get; set; }
+        public Point(int xPos, int yPos)
+        {
+            X = xPos;
+            Y = yPos;
+        }
+        public override string ToString() => $"[{this.X}, {this.Y}]";
+    }
+}
