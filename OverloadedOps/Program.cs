5845f09d6cb9eccf1c584c28203cd0c60ea8d921
Sergey Misko <spy_3462@mail.ru>
2022-07-11 17:06:36 +0400

    проверил работу перегруженных операторов + и - класса Point

--- OverloadedOps/Program.cs
+++ OverloadedOps/Program.cs
@@ -12,6 +12,12 @@ namespace OverloadedOps
             Point pointTwo = new Point(40, 40);
             Console.WriteLine("Point one = {0}",pointOne);
             Console.WriteLine("Point two = {0}", pointTwo);
+            // сложить 2 точки
+            Console.WriteLine("point one + point two: {0}",pointOne+pointTwo);
+            // вычесть одну точку из другой pointOne - pointTwo
+            Console.WriteLine("point one - point two: {0}", pointOne-pointTwo);
+            // вычесть одну точку из другой pointTwo - pointOne
+            Console.WriteLine("point two - point one: {0}",pointTwo-pointOne);
         }
     }
 }
