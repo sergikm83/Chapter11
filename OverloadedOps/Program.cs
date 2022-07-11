a50f9c8ca6989bf667846530c6489641ee74ac7a
Sergey Misko <spy_3462@mail.ru>
2022-07-11 16:50:10 +0400

    создал 2 точки и вывел на экран их координаты

--- OverloadedOps/Program.cs
+++ OverloadedOps/Program.cs
@@ -6,7 +6,12 @@ namespace OverloadedOps
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("***** Fin with Overloaded Operators *****\n");
+            // создать 2 точки
+            Point pointOne = new Point(100, 100);
+            Point pointTwo = new Point(40, 40);
+            Console.WriteLine("Point one = {0}",pointOne);
+            Console.WriteLine("Point two = {0}", pointTwo);
         }
     }
 }
