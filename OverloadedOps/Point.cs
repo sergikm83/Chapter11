45e261bdef91107ad191dddf19d88987c5bc8ee0
Sergey Misko <spy_3462@mail.ru>
2022-07-11 17:02:41 +0400

    перегрузил операторы + и - в классе Point

--- OverloadedOps/Point.cs
+++ OverloadedOps/Point.cs
@@ -16,5 +16,8 @@ namespace OverloadedOps
             Y = yPos;
         }
         public override string ToString() => $"[{this.X}, {this.Y}]";
+
+        public static Point operator +(Point p1, Point p2) => new Point(p1.X + p2.X, p1.Y + p2.Y);
+        public static Point operator -(Point p1, Point p2) => new Point(p1.X - p2.X, p1.Y - p2.Y);
     }
 }
