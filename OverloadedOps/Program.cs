af605e145172cd2e17d56aa1163c8afd87f77c03
Sergey Misko <spy_3462@mail.ru>
2022-07-12 16:54:55 +0400

    проверка операции равенства ==

--- OverloadedOps/Program.cs
+++ OverloadedOps/Program.cs
@@ -10,20 +10,21 @@ namespace OverloadedOps
             // создать 2 точки
             Point pointOne = new Point(100, 100);
             Point pointTwo = new Point(40, 40);
-            Console.WriteLine("Point one = {0}",pointOne);
+            Console.WriteLine("Point one = {0}", pointOne);
             Console.WriteLine("Point two = {0}", pointTwo);
             // сложить 2 точки
-            Console.WriteLine("point one + point two: {0}",pointOne+pointTwo);
+            Console.WriteLine("point one + point two: {0}", pointOne + pointTwo);
             // вычесть одну точку из другой pointOne - pointTwo
-            Console.WriteLine("point one - point two: {0}", pointOne-pointTwo);
+            Console.WriteLine("point one - point two: {0}", pointOne - pointTwo);
             // вычесть одну точку из другой pointTwo - pointOne
-            Console.WriteLine("point two - point one: {0}",pointTwo-pointOne);
+            Console.WriteLine("point two - point one: {0}", pointTwo - pointOne);
             Point pointThree = pointOne + 25;
             Console.WriteLine("point one + 25 = {0}", pointThree);
-            Console.WriteLine("17 + point three = {0}", 17+ pointThree);
+            Console.WriteLine("17 + point three = {0}", 17 + pointThree);
             Point pointFour = new Point(0, 500);
             pointFour += pointThree;
-            Console.WriteLine("point four + point three = {0}",pointFour);
+            Console.WriteLine("point four + point three = {0}", pointFour);
+            Console.WriteLine("point one == point two: {0}", pointOne == pointTwo);
         }
     }
 }
