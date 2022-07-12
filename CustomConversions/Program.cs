663c2c02fbc98e6f3b5276983b84b9389de7d4c5
Sergey Misko <sergik.m@outlook.com>
2022-07-12 22:26:20 +0500

    проверил работу структур Square и Rectangle

--- CustomConversions/Program.cs
+++ CustomConversions/Program.cs
@@ -6,7 +6,16 @@ namespace CustomConversions
     {
         static void Main(string[] args)
         {
-            Console.WriteLine("Hello World!");
+            Console.WriteLine("***** Fun with Conversions *****\n");
+            // Создать экземпляр Rectangle
+            Rectangle r = new Rectangle(15, 4);
+            Console.WriteLine(r.ToString());
+            r.Draw();
+            Console.WriteLine();
+            // Преобразовать r в Square на основе высоты Rectangle
+            Square s = (Square)r;
+            Console.WriteLine(s.ToString());
+            s.Draw();
         }
     }
 }
