af53cd1ff19ea99b2ca7bb4bef7c985102c56722
Sergey Misko <spy_3462@mail.ru>
2022-07-12 17:58:36 +0400

    добавил структуру Square и добавил методе явного преобразования Rectangle в Square

---
+++ CustomConversions/Square.cs
@@ -0,0 +1,33 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace CustomConversions
+{
+    public struct Square
+    {
+        public int Length { get; set; }
+        public Square(int l):this()
+        {
+            Length = l;
+        }
+        public void Draw()
+        {
+            for(int i=0;i<Length;i++)
+            {
+                for (int j=0;j<Length;j++)
+                    Console.Write("*");
+                Console.WriteLine();
+            }
+        }
+        public override string ToString() => $"[Length = {Length}]";
+        // Rectangle можно явно преобразовать в Square.
+        public static explicit operator Square(Rectangle r)
+        {
+            Square s = new Square { Length = r.Height };
+            return s;
+        }
+    }
+}
